46cf25b395b252ed3e084d6acd82af694a024efa
Esther Guo <special0831qi@gmail.com>
2023-12-18 13:13:47 +0800

    ico ui for mobile

--- app/ico/page.tsx
+++ app/ico/page.tsx
@@ -7,8 +7,8 @@ export default function ICO() {
   return (
     <div className="min-h-screen bg-white w-full">
         <main>
-            <section className="px-20 py-10 bg-[#FFC102] relative w-full flex justify-end">
-                <div className="absolute left-20 bottom-10">
+            <section className="px-4 md:px-20 py-10 bg-[#FFC102] relative w-full flex justify-center md:justify-end">
+                <div className="hidden md:block absolute left-20 bottom-10">
                 <Image
                 src="/ico.png"
                 alt="ICO background"
@@ -17,27 +17,27 @@ export default function ICO() {
                 />
                 </div>
         
-                <div className="bg-[#ffe49c] border-black border-[1px] min-w-[40%] py-10 px-12 rounded-xl shadow-md ">
+                <div className="bg-[#ffe49c] border-black border-[1px] min-w-[40%] py-6 md:py-10 px-6 md:px-12 rounded-xl shadow-md ">
                     <div className="font-semibold">Pre ICO Ends in:</div>
-                    <div className="flex justify-center space-x-2 mt-6 text-white text-center items-center">
-                        <div className="bg-[#7a5c47] p-4 rounded-xl">
-                            <div className="text-4xl font-bold mb-2 font-mono">30</div>
-                            <div className="text-sm">DAYS</div>
+                    <div className="flex justify-center space-x-2 mt-4 md:mt-6 text-white text-center items-center">
+                        <div className="bg-[#7a5c47] px-2 py-4 md:p-4 rounded-xl">
+                            <div className="text-3xl md:text-4xl font-bold mb-1 md:mb-2 font-mono">30</div>
+                            <div className="text-xs md:text-sm">DAYS</div>
                         </div>
-                        <div className="text-[#7a5c47] text-4xl">:</div>
-                        <div className="bg-[#7a5c47] p-4 rounded-xl">
-                            <div className="text-4xl font-bold mb-2 font-mono">00</div>
-                            <div className="text-sm">HOURS</div>
+                        <div className="text-[#7a5c47] text-2xl md:text-4xl">:</div>
+                        <div className="bg-[#7a5c47] px-2 py-4 md:p-4 rounded-xl">
+                            <div className="text-3xl md:text-4xl font-bold mb-1 md:mb-2 font-mono">00</div>
+                            <div className="text-xs md:text-sm">HOURS</div>
                         </div>
-                        <div className="text-[#7a5c47] text-4xl">:</div>
-                        <div className="bg-[#7a5c47] p-4 rounded-xl">
-                            <div className="text-4xl font-bold mb-2 font-mono">00</div>
-                            <div className="text-sm">MIN</div>
+                        <div className="text-[#7a5c47] text-2xl md:text-4xl">:</div>
+                        <div className="bg-[#7a5c47] px-2 py-4 md:p-4 rounded-xl">
+                            <div className="text-3xl md:text-4xl font-bold mb-1 md:mb-2 font-mono">00</div>
+                            <div className="text-xs md:text-sm">MIN</div>
                         </div>
-                        <div className="text-[#7a5c47] text-4xl">:</div>
-                        <div className="bg-[#7a5c47] p-4 rounded-xl">
-                            <div className="text-4xl font-bold mb-2 font-mono">00</div>
-                            <div className="text-sm">SEC</div>
+                        <div className="text-[#7a5c47] text-2xl md:text-4xl">:</div>
+                        <div className="bg-[#7a5c47] px-2 py-4 md:p-4 rounded-xl">
+                            <div className="text-3xl md:text-4xl font-bold mb-1 md:mb-2 font-mono">00</div>
+                            <div className="text-xs md:text-sm">SEC</div>
                         </div>
                     </div>
                     <div className="w-full h-2 bg-[#7a5c47] rounded mt-4">
@@ -66,8 +66,8 @@ export default function ICO() {
                 </div>
             </section>
         
-            <div className="bg-[#fff4cc] border-black border-[1px] mx-24 my-16 py-6 px-36 rounded-xl shadow-md">
-                <div className="grid grid-cols-2 gap-4 place-content-around font-semibold">
+            <div className="bg-[#fff4cc] border-black border-[1px] mx-4 md:mx-24 my-12 md:my-16 py-6 px-4 md:px-36 rounded-xl shadow-md">
+                <div className="grid grid-cols-2 gap-4 place-content-around md:font-semibold">
                     <div>OPE to be claimed</div>
                     <div className="text-right">XXX</div>
                     <div>Status</div>
@@ -75,9 +75,9 @@ export default function ICO() {
                     <div>Sale type</div>
                     <div className="text-right">public</div>
                     <div>Minimum Buy</div>
-                    <div className="text-right">0.1KKC</div>
+                    <div className="text-right">0.1 KKC</div>
                     <div>Maximum Buy</div>
-                    <div className="text-right">10000KKC</div>
+                    <div className="text-right">10,000 KKC</div>
                     <div>The first round quota 30%</div>
                     <div className="text-right">0.05U</div>
                     <div>The second round quota 70%</div>
